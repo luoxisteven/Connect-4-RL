b8f1303b19b47bbd23544f24d33734c011fcbfd6
Xi Luo <429066012@qq.com>
2025-12-03 18:08:47 +1100

    Update Connect 4

--- RL/Connect4.py
+++ RL/Connect4.py
@@ -1,19 +1,27 @@
 class Connect4:
     def __init__(self):
-        # self.board 
-        # self.current_player 
-        # self.game_over 
-        # self.winner
-        return
+        self.board = [["_" for _ in range(7)] for _ in range(6)]
+        self.current_player = "x"
+        self.game_over = False
+        self.winner = None
+        # self.action = (player, column_index)
 
-    def get_state(self):
-        return
+    def get_current_state(self):
+        return self.board
     
-    def get_legal_moves(self):
-        return
-    
-    def take_action(self, column):
-        return
+    def is_legal_action(self, action):
+        if self.board[0][action[1]] != "_":
+            return False
+        return True
+
+    def take_action(self, action):
+        if not self.is_legat_action(action):
+            return False
+        else:
+            for i in range(5,0,-1):
+                if self.board[i][action[1]] == "_":
+                    self.board[i][action[1]] == action[0]
+                    return True
 
     def check_winner(self):
         return
@@ -25,4 +33,10 @@ class Connect4:
         return
     
     def get_reward(self):
+        return
+
+    def get_start_state(self):
+        return
+    
+    def is_goal_state(self):
         return
---
+++ RL/test.py
@@ -0,0 +1,5 @@
+# a = (1 ,2)
+# print(a[0])
+
+for i in range(6,0,-1):
+    print(i)
