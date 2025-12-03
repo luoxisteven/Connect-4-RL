9999a2b979c26f94b8e5b540fefe0fce3151e007
Xi Luo <429066012@qq.com>
2025-12-03 17:26:36 +1100

    Update Connect 4 Python Instance

---
+++ RL/Connect4.py
@@ -0,0 +1,28 @@
+class Connect4:
+    def __init__(self):
+        # self.board 
+        # self.current_player 
+        # self.game_over 
+        # self.winner
+        return
+
+    def get_state(self):
+        return
+    
+    def get_legal_moves(self):
+        return
+    
+    def take_action(self, column):
+        return
+
+    def check_winner(self):
+        return
+    
+    def reset_game(self):
+        return
+    
+    def switch_player(self):
+        return
+    
+    def get_reward(self):
+        return
