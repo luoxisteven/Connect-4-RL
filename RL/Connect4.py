97ee2c521c865e6e4e99542e54953c8c3fabd553
Xi Luo <429066012@qq.com>
2025-12-04 16:47:44 +1100

    Update Connect-4

--- RL/Connect4.py
+++ RL/Connect4.py
@@ -1,16 +1,28 @@
 class Connect4:
-    def __init__(self):
-        self.board = [["_" for _ in range(7)] for _ in range(6)]
+    def __init__(self, board):
+        self.height = 6
+        self.width = 7
+        # Start from (0,0) the bottom left similar to regular (x,y) coordinates
+        # a[x][y] = Location(x,y) start from the bottom left
+        self.board = board or [["_" for _ in range(self.height)] for _ in range(self.width)]
         self.current_player = "x"
         self.game_over = False
         self.winner = None
         # self.action = (player, column_index)
-
-    def get_current_state(self):
-        return self.board
     
-    def is_legal_action(self, action):
-        if self.board[0][action[1]] != "_":
+    def is_legal_action(self, action, state=None):
+        if state is None:
+            state = self.board
+        if self.game_over:
+            return False
+        action_player = action[0]
+        action_col = action[1]
+        if action_player != self.current_player:
+            return False
+        if self.board[action_col][self.height-1] != "_" \
+            or (not isinstance(action_col, int)) \
+            or (action_col < 0) \
+            or (action_col > self.width-1):
             return False
         return True
 
@@ -18,26 +30,110 @@ class Connect4:
         if not self.is_legal_action(action):
             return False
         else:
-            for i in range(5,-1,-1):
-                if self.board[i][action[1]] == "_":
-                    self.board[i][action[1]] = action[0]
+            for i in range(self.height):
+                if self.board[action[1]][i] == "_":
+                    self.board[action[1]][i] = action[0]
+                    self.switch_player()
                     break
             return True
 
-    def check_winner(self):
-        return
+    def take_search_action(self, action, state=None):
+        if state is None:
+            state = self.board
+        if not self.is_legal_action(action, state):
+            return False
+        else:
+            for i in range(self.height):
+                if self.board[action[1]][i] == "_":
+                    self.board[action[1]][i] = action[0]
+                    break
+            return True
+
+    def check_winner(self, board=None):
+        if board is None:
+            board = self.board
+        # Vertical check
+        for x in range(self.width):
+            for y in range(self.height - 3):
+                player = board[x][y]
+                if player == "_":
+                    break
+                if player == board[x][y+1] == board[x][y+2] == board[x][y+3]:
+                    self.game_over = True
+                    self.winner = player
+                    return player  
+
+        # Horizontal check
+        for x in range(self.width - 3):
+            for y in range(self.height):
+                player = board[x][y]
+                if player == "_":
+                    break
+                if player == board[x+1][y] == board[x+2][y] == board[x+3][y]:
+                    self.game_over = True
+                    self.winner = player
+                    return player
+
+        # Diagonal down-right
+        for x in range(self.width - 3):
+            for y in range(self.height - 3):
+                player = board[x][y]
+                if player == "_":
+                    break
+                if player != "_" and player == board[x+1][y+1] == board[x+2][y+2] == board[x+3][y+3]:
+                    self.game_over = True
+                    self.winner = player
+                    return player
+
+        # Diagonal up-right
+        for x in range(3, self.width):
+            for y in range(self.height - 3):
+                player = board[x][y]
+                if player == "_":
+                    break
+                if player != "_" and player == board[x-1][y+1] == board[x-2][y+2] == board[x-3][y+3]:
+                    self.game_over = True
+                    self.winner = player
+                    return player
+
+        return None
     
     def reset_game(self):
+        self.board = [["_" for _ in range(self.height)] for _ in range(self.width)]
         return
     
     def switch_player(self):
-        return
+        self.current_player = "o" if self.current_player == "x" else "x"
     
-    def get_reward(self):
-        return
+    def print_board(self):
+        for y in reversed(range(self.height)):
+            row = []
+            for x in range(self.width):
+                row.append(self.board[x][y])
+            print(" ".join(row))
 
-    def get_start_state(self):
-        return
-    
-    def is_goal_state(self):
-        return
+    # RL-related methods
+    def get_current_state(self):
+        return self.board
+
+    def get_next_state(self, state, action):
+        if state is None:
+            state = self.board
+        next_state = self.take_search_action(action, state)
+        return next_state
+
+    def is_goal_state(self, board=None):
+        if board is None:
+            board = self.board
+        if self.check_winner(board) is not None:
+            return True
+        return False
+
+    def get_reward(self, action, state=None, next_state=None):
+        if state is None:
+            state = self.board
+        if next_state is None:
+            next_state = self.get_next_state(state, action)
+        if self.is_goal_state(next_state):
+            if self.winner == self.current_player:
+                return 1
--- RL/test.py
+++ RL/test.py
@@ -1,5 +1,76 @@
-# a = (1 ,2)
-# print(a[0])
+import unittest
+import Connect4
 
-for i in range(6,0,-1):
-    print(i)
+
+class TestCheckWinner(unittest.TestCase):
+    def test_no_winner_on_empty_board(self):
+        game = Connect4.Connect4(None)
+        self.assertIsNone(game.check_winner())
+        self.assertFalse(game.game_over)
+        self.assertIsNone(game.winner)
+
+    def test_vertical_win(self):
+        game = Connect4.Connect4(None)
+        for _ in range(4):
+            self.assertTrue(game.take_action(("x", 0)))
+
+        self.assertEqual(game.check_winner(), "x")
+        self.assertTrue(game.game_over)
+        self.assertEqual(game.winner, "x")
+
+    def test_horizontal_win(self):
+        game = Connect4.Connect4(None)
+        for col in range(4):
+            self.assertTrue(game.take_action(("x", col)))
+
+        self.assertEqual(game.check_winner(), "x")
+        self.assertTrue(game.game_over)
+        self.assertEqual(game.winner, "x")
+
+    def test_diagonal_down_right_win(self):
+        game = Connect4.Connect4(None)
+
+        # Build diagonal from (0,0) to (3,3)
+        game.take_action(("x", 0))  # (0,0)
+
+        game.take_action(("o", 1))  # (1,0)
+        game.take_action(("x", 1))  # (1,1)
+
+        game.take_action(("o", 2))  # (2,0)
+        game.take_action(("o", 2))  # (2,1)
+        game.take_action(("x", 2))  # (2,2)
+
+        game.take_action(("o", 3))  # (3,0)
+        game.take_action(("o", 3))  # (3,1)
+        game.take_action(("o", 3))  # (3,2)
+        game.take_action(("x", 3))  # (3,3)
+
+        self.assertEqual(game.check_winner(), "x")
+        self.assertTrue(game.game_over)
+        self.assertEqual(game.winner, "x")
+
+    def test_diagonal_up_right_win(self):
+        game = Connect4.Connect4(None)
+
+        # Build diagonal from (0,3) up-right to (3,0)
+        game.take_action(("x", 3))  # (3,0)
+
+        game.take_action(("o", 2))  # (2,0)
+        game.take_action(("x", 2))  # (2,1)
+
+        game.take_action(("o", 1))  # (1,0)
+        game.take_action(("o", 1))  # (1,1)
+        game.take_action(("x", 1))  # (1,2)
+
+        game.take_action(("o", 0))  # (0,0)
+        game.take_action(("o", 0))  # (0,1)
+        game.take_action(("o", 0))  # (0,2)
+        game.take_action(("x", 0))  # (0,3)
+
+        self.assertEqual(game.check_winner(), "x")
+        self.assertTrue(game.game_over)
+        self.assertEqual(game.winner, "x")
+
+
+if __name__ == "__main__":
+    unittest.main()
