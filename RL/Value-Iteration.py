bf2e7c50cb5a1ba5f5b586fcd236c14adb8d6031
Xi Luo <429066012@qq.com>
2025-12-05 13:44:15 +1100

    Update RL

--- RL/Value-Iteration.py
+++ RL/Value-Iteration.py
@@ -1,6 +1,11 @@
+import Connect4
+
 class ValueIteration:
     def __init__(self):
+        self.game = Connect4.Connect4()
         self.q_table = {}
         self.discount_factor = 0.9
-        
+    
+    def value_iteration(self):
+        return
         
